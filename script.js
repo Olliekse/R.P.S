3077a1ffccfe0cff8337a0995df725213eb9d274
Oliver Richardson <123179922+Olliekse@users.noreply.github.com>
2023-09-13 01:21:14 +0300

    Refactor variables

--- script.js
+++ script.js
@@ -3,32 +3,21 @@
 
 const game = () => {
   for (i = 0; i <= 4; i++) {
-    const playerSelection = prompt("Rock, Paper or Scissors?");
-    const getComputerChoice = Math.floor(Math.random() * 3) + 1;
-    const computerSelection = getComputerChoice;
+    const playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
+    const choices = ["Rock", "Paper", "Scissors"];
+    const computerSelection =
+      choices[Math.floor(Math.random() * choices.length)];
 
     const playRound = (playerSelection, computerSelection) => {
-      if (playerSelection.toLowerCase() === "rock" && computerSelection === 2) {
+      if (playerSelection === "rock" && computerSelection === 2) {
         return "You lose! Paper beats Rock";
-      } else if (
-        playerSelection.toLowerCase() === "paper" &&
-        computerSelection === 3
-      ) {
+      } else if (playerSelection === "paper" && computerSelection === 3) {
         return "You lose! Scissors beat Paper";
-      } else if (
-        playerSelection.toLowerCase() === "scissors" &&
-        computerSelection === 1
-      ) {
+      } else if (playerSelection === "scissors" && computerSelection === 1) {
         return "You lose! Rock beats Scissors";
-      } else if (
-        playerSelection.toLowerCase() === "rock" &&
-        computerSelection === 3
-      ) {
+      } else if (playerSelection === "rock" && computerSelection === 3) {
         return "You win! Rock beats scissors";
-      } else if (
-        playerSelection.toLowerCase() === "paper" &&
-        computerSelection === 1
-      ) {
+      } else if (playerSelection === "paper" && computerSelection === 1) {
         return "You win! Paper beats Rock";
       } else {
         return "It's a draw!";
